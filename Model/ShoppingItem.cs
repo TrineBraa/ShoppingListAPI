11cda65df1f5a0595a7c6b25f215da7dab2f74bc
trinebra <157688079+trinebra@users.noreply.github.com>
2024-10-14 12:38:25 +0200

    Add project files.

---
+++ Model/ShoppingItem.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoppingList.Model
+{
+    public class ShoppingItem
+    {
+        public Guid id { get; set; }
+        public string Shopping { get; set; }
+
+        public ShoppingItem(string shopping) : this()
+        {
+            Shopping = shopping;
+        }
+
+        public ShoppingItem()
+        {
+            id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+using System.Data.SqlClient;
+using Dapper;
+using ShoppingList.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+const string connStr = @"Data Source=DESKTOP-1HV37H6\SQLEXPRESS;Initial Catalog=ShoppingList;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/shoppingList", async () =>
+{
+        var connection = new SqlConnection(connStr);
+        const string SQL = "select id, Shopping FROM shoppingList";
+        var shoppingItems = await connection.QueryAsync<ShoppingItem>(SQL);
+        return shoppingItems;
+});
+
+app.MapPost("/shoppingList", async (ShoppingItem shoppingItem) =>
+{
+    var connection = new SqlConnection(connStr);
+    const string SQL = "INSERT INTO shoppingList (id, Shopping) VALUES (@id, @Shopping)";
+    var rowsAffects = await connection.ExecuteAsync(SQL, shoppingItem);
+    return rowsAffects;
+});
+
+app.MapPut("/shoppingList", async (ShoppingItem shoppingItem) =>
+{
+    var connection = new SqlConnection(connStr);
+    const string SQL = "UPDATE shoppingList Set Shopping = @Shopping WHERE id = @id";
+    var rowsAffected = await connection.ExecuteAsync(SQL, shoppingItem);
+    return rowsAffected;
+});
+
+app.MapDelete("/shoppingList/{id}", async (Guid id) =>
+{
+    var connection = new SqlConnection(connStr);
+    const string SQL = "DELETE FROM shoppingList WHERE id = @id";
+    var rowsAffected = await connection.ExecuteAsync(SQL, new { id = id });
+    return rowsAffected;
+});
+
+app.UseStaticFiles();
+app.Run();
+
